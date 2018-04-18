fd30b06cdc0b022c996ef5b4551799308401acfd
dmitriy70494 <d89086362742@yandex.ru>
2018-04-18 16:09:16 +0300

    1.3. JavaDoc. Code convention [#50066]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,21 @@
+/**
+ * Class Calculatе
+ * Вывод приветствия в консоль
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version 1
+ * @since 18.4.2018
+*/
 package ru.job4j;
 
 class Calculate{
-public static void main(String[] args)
-{
-System.out.println("hellou world");
-} 
+/**
+ * Method Main
+ * Метод выводит приветствие в консоль
+ * @param args - args
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @since 18.4.2018
+*/
+ public static void main(final String[] args){
+  System.out.println("hellou world");
+ } 
 }
