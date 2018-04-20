a2030662cbe20b0c95caa9adae469d1dfd81ca23
dmitriy70494 <d89086362742@yandex.ru>
2018-04-20 15:02:09 +0300

    5.5. Слова начинается с ...[#50371]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 20.04.2018
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return true если слово начинаеться с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i != value.length; i++) {
+            if (!(value[i] == data[i])) {
+                result = false;
+                break;
+            }
+        }
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        return result;
+    }
+}
