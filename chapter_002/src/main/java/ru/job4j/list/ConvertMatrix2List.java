088b1cb63f479c5753b108436fcddee5162b4c18
dmitriy70494 <d89086362742@yandex.ru>
2018-05-01 18:43:24 +0300

    3. Конвертация двумерного массива в ArrayList[#51698]

---
+++ chapter_002/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,30 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Класс ConvertList2Array. Делает из двумерного массива список
+ *
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 01.05.2018
+ */
+public class ConvertMatrix2List {
+
+    /**
+     * В метод приходит двумерный массив целых чисел, метод проходится
+     * по всем элементам массива и добавляет их в List<Integer>
+     *
+     * @param array двумерный массив
+     * @return list список
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] mas : array) {
+            for (int number : mas) {
+                list.add(number);
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тесты для класса ConvertMatrix2List.
+ *
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 01.05.2018
+ */
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when3on4ArrayThenList12() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12},
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
