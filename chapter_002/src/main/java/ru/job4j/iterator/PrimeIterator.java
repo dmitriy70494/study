51eb48ab2b7b9067903d28c6be26b8f7b2f54763
dmitriy70494 <d89086362742@yandex.ru>
2018-05-13 15:58:12 +0300

    5.1.3. Создать итератор простые числа.[#53099]

---
+++ chapter_002/src/main/java/ru/job4j/iterator/PrimeIterator.java
@@ -0,0 +1,99 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.BitSet;
+
+/**
+ * класс PrimeIterator. Реализация Итератора получающего простые числа.
+ *
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 14.05.2018
+ */
+public class PrimeIterator implements Iterator {
+
+    /**
+     * массив.
+     */
+    private final int[] mas;
+
+    /**
+     * Длина массива.
+     */
+    private final int length;
+
+    /**
+     * Индекс элемента на котором находится корретка метода next и hasNext.
+     */
+    private int cursor = 0;
+
+    /**
+     * Содержит все простые числа от 2 до самого максимального числа в массиве.
+     */
+    private BitSet primes;
+
+    /**
+     * Конструктор инициализирует все необходимые параметры,
+     * Инициализирует обрабатываемый массив mas
+     * Инициализирует длину массива для проверок следующего хода
+     * Далее определяет максимальное число в массиве
+     * BitSet заполняется всеми простыми числами от 2 до максимального числа в массиве включая его
+     *
+     * @param mas исходный массив
+     */
+    public PrimeIterator(int[] mas) {
+        this.mas = mas;
+        this.length = this.mas.length;
+
+        int max = this.mas[0];
+        for (int number : mas) {
+            if (max < number) {
+                max = number;
+            }
+        }
+
+        primes = new BitSet(max + 1);
+        for (int number = 2; number <= max; number++) {
+            primes.set(number);
+        }
+        int prime = 2;
+        while (prime * prime <= max) {
+            if (primes.get(prime)) {
+                int notPrime = prime * prime;
+                while (notPrime <= max) {
+                    primes.clear(notPrime);
+                    notPrime += prime;
+                }
+            }
+            prime++;
+        }
+    }
+
+    /**
+     * Метод проверяет можно ли считать число, если нельзя, выбрасывает исключение
+     * затем считывает число переводя корретку дальше.
+     *
+     * @return следующее число в массиве
+     */
+    public Integer next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return this.mas[this.cursor++];
+    }
+
+    /**
+     * Определяет не закончился ли массив и есть ли следующий элемент
+     * также проталкивает корретку, если число не содержится в списке простых чисел
+     *
+     * @return true элемент есть, false элемента нет
+     */
+    public boolean hasNext() {
+        boolean access = this.cursor != this.length;
+        while (access && !primes.get(mas[cursor])) {
+            access = ++this.cursor != this.length;
+        }
+        return access;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/iterator/PrimeIteratorTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * класс PrimeIteratorTest. Проверка Итератора получающего простые числа.
+ *
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 14.05.2018
+ */
+public class PrimeIteratorTest {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new PrimeIterator(new int[]{1, 2, 3, 4, 5, 6, 7, 3571});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnPrimeNumbersOnly() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3571));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(7));
+        assertThat(it.next(), is(3571));
+    }
+
+    @Test
+    public void shouldReturnFalseCauseThereIsNoAnyPrimeNumber() {
+        it = new PrimeIterator(new int[]{4, 6});
+        assertThat("should return false, cause there is no any prime number", it.hasNext(), is(false));
+    }
+}
