ce948544c0616e0b5f96ab2b2e93d84592ce376d
dmitriy70494 <d89086362742@yandex.ru>
2018-05-17 07:53:43 +0300

    1. Создать модель User[#53552]

---
+++ chapter_002/src/main/java/ru/job4j/simplemap/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.simplemap;
+
+import java.util.Calendar;
+
+/**
+ * класс User. Хранит 3 поля данных о пользователе.
+ *
+ * @author Dmitriy Balandin (d89086362742@yandex.ru)
+ * @version $Id$
+ * @since 15.05.2018
+ */
+public class User {
+
+    String name;
+
+    int children;
+
+    Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
