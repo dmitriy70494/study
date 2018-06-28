1a46dd6166317bf61cc9eabcddc91d2f72ab8d1e
Dmitriy Balandin <d89086362742@yandex.ru>
2018-06-28 08:34:55 +0500

    home

--- chapter_008/src/main/java/ru/job4j/servlets/JsonControllers.java
+++ chapter_008/src/main/java/ru/job4j/servlets/JsonControllers.java
@@ -4,10 +4,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JsonControllers extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/json");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("[{\"name\" : \"name2\", \"lastname\" : \"lastname2\"}, {\"name\" : \"name2\", \"lastname\" : \"lastname2\"}]");
+        writer.flush();
     }
+
+
 }
